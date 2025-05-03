d4d2364f85f2efc6073b95e202bc13377d693931
camigrassi04 <camigras04@gmail.com>
2025-05-03 20:38:38 +0000

    hechos a y b y cacho del c del recu 2024

--- primeros_parciales/primeros_parciales/parciales_viejos-2024/2c2024-r1/ej1/ej1.asm
+++ primeros_parciales/primeros_parciales/parciales_viejos-2024/2c2024-r1/ej1/ej1.asm
@@ -1,4 +1,5 @@
 extern malloc
+extern free
 
 section .rodata
 ; Acá se pueden poner todas las máscaras y datos que necesiten para el ejercicio
@@ -9,33 +10,37 @@ FALSE EQU 0
 ; Marca un ejercicio como hecho
 TRUE  EQU 1
 
+FILAS EQU 255
+
+COLUMNAS EQU 255
+
 ; Marca el ejercicio 1A como hecho (`true`) o pendiente (`false`).
 ;
 ; Funciones a implementar:
 ;   - optimizar
 global EJERCICIO_1A_HECHO
-EJERCICIO_1A_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_1A_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ; Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
 ;
 ; Funciones a implementar:
 ;   - contarCombustibleAsignado
 global EJERCICIO_1B_HECHO
-EJERCICIO_1B_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_1B_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ; Marca el ejercicio 1C como hecho (`true`) o pendiente (`false`).
 ;
 ; Funciones a implementar:
 ;   - modificarUnidad
 global EJERCICIO_1C_HECHO
-EJERCICIO_1C_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_1C_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
-ATTACKUNIT_CLASE EQU ??
-ATTACKUNIT_COMBUSTIBLE EQU ??
-ATTACKUNIT_REFERENCES EQU ??
-ATTACKUNIT_SIZE EQU ??
+ATTACKUNIT_CLASE EQU 0
+ATTACKUNIT_COMBUSTIBLE EQU 12
+ATTACKUNIT_REFERENCES EQU 14
+ATTACKUNIT_SIZE EQU 16
 
 global optimizar
 optimizar:
@@ -43,21 +48,216 @@ optimizar:
 	; ubicación según la convención de llamada. Prestá atención a qué
 	; valores son de 64 bits y qué valores son de 32 bits o 8 bits.
 	;
-	; r/m64 = mapa_t           mapa
-	; r/m64 = attackunit_t*    compartida
-	; r/m64 = uint32_t*        fun_hash(attackunit_t*)
+	; rdi = mapa_t           mapa
+	; rsi = attackunit_t*    compartida
+	; rdx = uint32_t*        fun_hash(attackunit_t*)
+
+	; prólogo
+	push rbp
+	mov rbp, rsp
+
+	; preservo registros no volátiles
+	push r12
+	push r13
+	push r14
+	push r15
+	push rbx
+
+	; preservo parámetros de la función
+	mov r12, rdi ; mapa_t
+	; en rdi voy a tener mapa_t[0][0]
+	mov r13, rsi ; compartida
+	mov r14, rdx ; funhash
+
+	mov rdi, r13
+	sub rsp, 8
+	call r14 ; eax = hash_compartida
+
+	mov r15d, eax ; preservo hash_compartida en un registro no volatil
+
+	xor rbx, rbx ; i = 0
+
+.loop:
+	cmp rbx, FILAS * COLUMNAS
+	je .fin
+
+	mov rdi, [r12] ;attackunit_t* (mapa[i][j])
+
+	cmp rdi, 0 ; rdi == null pointer?
+	je .next_iteration
+
+	cmp rdi, r13
+	je .next_iteration
+
+	call r14 ; eax = fun_hash(mapa[i][j])
+
+	cmp r15d, eax ; hash_actual == hash_compartida?
+	jne .next_iteration
+
+	mov rdi, [r12]
+	dec BYTE [rdi + ATTACKUNIT_REFERENCES] ; mapa[i][j]->references--
+	inc BYTE [r13 + ATTACKUNIT_REFERENCES] ; compartida->references++
+	mov [r12], r13 ; mapa[i][j] = compartida
+
+	cmp BYTE [rdi + ATTACKUNIT_REFERENCES], 0
+	jne .next_iteration
+
+	call free
+
+.next_iteration:
+	inc rbx
+	add r12, 8
+	jmp .loop
+
+.fin:
+	; epílogo
+	add rsp, 8
+	pop rbx
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop rbp
 	ret
 
 global contarCombustibleAsignado
 contarCombustibleAsignado:
-	; r/m64 = mapa_t           mapa
-	; r/m64 = uint16_t*        fun_combustible(char*)
+	; rdi = mapa_t           mapa
+	; rsi = uint16_t*        fun_combustible(char*)
+
+	; prologo
+	push rbp
+	mov rbp, rsp
+
+	push r12
+	push r13
+	push r14
+	push r15
+	push rbx
+
+	mov r12, rdi
+	mov r13, rsi
+
+	xor rbx, rbx ; i
+	xor r15, r15 ; acum (r15d)
+
+.loop:
+	cmp rbx, FILAS * COLUMNAS
+	je .fin
+
+	mov rdi, [r12] ; mapa[i][j]
+	cmp rdi, 0
+	je .next_iteration ; mapa[i][j] == 0 -> continue
+
+	sub rsp, 8
+	call r13 ; ax = comb_designado
+	add rsp, 8
+
+	movzx r8d, ax ; (uint32_t) comb_designado
+	mov rdi, [r12]
+	movzx r9d, WORD [rdi + ATTACKUNIT_COMBUSTIBLE] ; mapa[i][j]->combustible
+	sub r9d, r8d
+	add r15d, r9d
+
+
+.next_iteration:
+	inc rbx
+	add r12, 8
+	jmp .loop
+
+.fin:
+	; epilogo
+	mov eax, r15d
+	pop rbx
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop rbp
 	ret
 
 global modificarUnidad
 modificarUnidad:
-	; r/m64 = mapa_t           mapa
-	; r/m8  = uint8_t          x
-	; r/m8  = uint8_t          y
-	; r/m64 = void*            fun_modificar(attackunit_t*)
+	; rdi = mapa_t           mapa
+	; sil  = uint8_t          x
+	; dil  = uint8_t          y
+	; rcx = void*            fun_modificar(attackunit_t*)
+
+	; prologo
+	push rbp
+	mov rbp, rsp
+
+	push r12
+	push r13
+	push r14
+	push r15
+	push rbx
+
+	mov r12, rdi ; mapa
+	movzx r13, sil ; x
+	movzx r14, dil ; y
+	mov r15, rcx ; fun_modificar
+
+	;mov r8, [r12]
+	;cmp r8, 0
+	;je .fin
+	
+	mov r8, r13 ; copio r13 en un registro auxiliar
+	imul r8, COLUMNAS ; x * COLUMNAS
+
+	mov r9, r14 ; r9 = y
+	add r9, r8
+
+	imul r9, 8 ; 8*(255*x + y)
+	add r12, r9
+
+	cmp [r12], 0
+	je .fin ; mapa[x][y] == null?
+
+	mov r8, [rbx + ATTACKUNIT_REFERENCES] ; mapa[x][y]->references
+	cmp r8, 1
+	je .funcion
+
+	dec r8
+	mov rdi, ATTACKUNIT_SIZE
+
+	sub rsp, 8
+	call malloc ; rax = puntero a nueva_unidad
+	add rsp, 8
+
+	mov [rax], [r12] ; *nueva_unidad = *unidad;
+	mov [rax + ATTACKUNIT_REFERENCES], BYTE 1 ; nueva_unidad->references = 1
+	mov [r12], rax ; mapa[x][y] = nueva_unidad
+
+.funcion:
+	mov rdi, rbx
+	sub rsp, 8
+	call r15
+	add rsp, 8
+
+.fin:
+	; epilogo
+	pop rbx
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop rbp
 	ret
+
+                                                                   
+;          mapa_t : attackunit_t**                                   
+;    ┌──────────────────────────────┐                                
+;    │x x x x x x ................. ┼─► 255 columnas                 
+;    │                              │   tamaño de c/celda: 8 bytes   
+;    │                              │                                
+;    └──────────────────────────────┘                                
+;    ej: mapa[2][3]                                                  
+;    para ubicarnos en toda la tercera fila y luego                  
+;    irnos a la 4ta columna, tendríamos que multiplicar x            
+;    por 255 (#columnas). después le sumamos el y                    
+;    (desplazamiento en la fila) y multiplicamos todo por            
+;    el tamaño del dato de las celdas.                               
+                                                                   
+;    nos queda: dirección_base + 8*(255*x + y)                       
+                                                                   
--- primeros_parciales/primeros_parciales/parciales_viejos-2024/2c2024-r1/ej1/ej1.c
+++ primeros_parciales/primeros_parciales/parciales_viejos-2024/2c2024-r1/ej1/ej1.c
@@ -10,7 +10,7 @@
  * Funciones a implementar:
  *   - es_indice_ordenado
  */
-bool EJERCICIO_1A_HECHO = false;
+bool EJERCICIO_1A_HECHO = true;
 
 /**
  * Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
@@ -18,7 +18,7 @@ bool EJERCICIO_1A_HECHO = false;
  * Funciones a implementar:
  *   - contarCombustibleAsignado
  */
-bool EJERCICIO_1B_HECHO = false;
+bool EJERCICIO_1B_HECHO = true;
 
 /**
  * Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
@@ -26,22 +26,97 @@ bool EJERCICIO_1B_HECHO = false;
  * Funciones a implementar:
  *   - modificarUnidad
  */
-bool EJERCICIO_1C_HECHO = false;
+bool EJERCICIO_1C_HECHO = true;
 
 /**
  * OPCIONAL: implementar en C
+ * 
+ *                                                                                    
+                                                                                   
+            mapa_t                     mapa[i][j]                                  
+     ┌──────────────┌──┐            ┌──────────────┐           ┌────────────┐      
+     │              │ ─┼──────────► │ attackunit_t*├────────►  │attackunit_t│      
+     │              │  │            │              │           │            │      
+     ┌──────────────└──┘            └──────────────┘           └────────────┘      
+     │                 │                      ▲                                    
+     │                 │                      │                                    
+     ┌─────────────────┐                      │                                    
+     │                 │                      │                                    
+     │                 │                      │                                    
+     ┌─────────────────┐     ┌────────────┐   │                                    
+     │                 │     │            │   │                                    
+     │               ──┼───► │ mapa_t[i]  ┼───┘                                    
+     └─────────────────┘     └────────────┘                                        
+                             attackunit_t**                                        
+        ;esta flecha no es un puntero, es lo que es.                                                                
+                                                                                   
+                                                                                   
  */
 void optimizar(mapa_t mapa, attackunit_t* compartida, uint32_t (*fun_hash)(attackunit_t*)) {
+    //idea: recorrer todas las posiciones del mapa y buscar que unidades tienen el mismo hash
+    //luego, modificar la referencia para que apunte a la compartida, disminuir de la unidad vieja en uno la referencia y aumentar en uno la referencia de la compartida
+
+    uint32_t hash_compartida = fun_hash(compartida);
+
+    for (int i = 0; i < 255; i++){
+        for (int j = 0; j < 255; j++){
+            //casos borde: si mapa[i][j] == compartida entonces no hago nada
+            if (mapa[i][j] == compartida){
+                continue;
+            }
+            //si mapa[i][j] no tiene un puntero a un attackunit
+            if (mapa[i][j] == 0){
+                continue;
+            }
+
+            uint32_t hash_particular = fun_hash(mapa[i][j]);
+            if (hash_particular != hash_compartida){
+                continue;
+            }
+            mapa[i][j]->references--;
+            if (mapa[i][j]->references == 0){
+                free(mapa[i][j]); //importante, podemos liberar solo cuando referencias está en 0
+            }
+            mapa[i][j] = compartida;
+            compartida->references++;
+        }
+    }
 }
 
 /**
  * OPCIONAL: implementar en C
  */
 uint32_t contarCombustibleAsignado(mapa_t mapa, uint16_t (*fun_combustible)(char*)) {
+    uint32_t acum = 0;
+    for (int i = 0; i < 255; i++){
+        for (int j = 0; j < 255; j++){
+            if (mapa[i][j] == 0){
+                continue;
+            }
+            uint32_t comb_designado = (uint32_t) fun_combustible(mapa[i][j]->clase);
+            uint32_t dif = (mapa[i][j]->combustible) - comb_designado;
+            acum += dif;
+        }   
+    }
+    return acum;
 }
 
 /**
  * OPCIONAL: implementar en C
  */
 void modificarUnidad(mapa_t mapa, uint8_t x, uint8_t y, void (*fun_modificar)(attackunit_t*)) {
+    attackunit_t* unidad = mapa[x][y];
+    if (unidad == NULL){
+        return;
+    }
+    //si la unidad tiene una referencia, la modificamos directamente
+    if (unidad->references > 1){
+        unidad->references--;
+        attackunit_t* nueva_unidad = malloc(sizeof(attackunit_t));
+        *nueva_unidad = *unidad;
+        nueva_unidad->references = 1;
+        mapa[x][y] = nueva_unidad;
+    } 
+    
+    fun_modificar(mapa[x][y]);
 }
