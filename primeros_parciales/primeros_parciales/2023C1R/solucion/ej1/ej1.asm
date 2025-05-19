beb3bf5e69cf4ce5235559b38634da01712287e1
camigrassi04 <camigras04@gmail.com>
2025-05-19 01:10:47 +0000

    arreglo ejercicio 1 2023C1R

--- primeros_parciales/primeros_parciales/2023C1R/solucion/ej1/ej1.asm
+++ primeros_parciales/primeros_parciales/2023C1R/solucion/ej1/ej1.asm
@@ -2,6 +2,10 @@ global acumuladoPorCliente_asm
 global en_blacklist_asm
 global blacklistComercios_asm
 
+extern malloc
+extern calloc
+extern strcmp
+
 %define CANTIDAD_DE_CLIENTES 10
 %define SIZE_OF_UINT32 4 
 ;son 32 bits, es decir, 4 bytes.
@@ -9,47 +13,111 @@ global blacklistComercios_asm
 ;########### SECCION DE TEXTO (PROGRAMA)
 section .text
 
-; cantidadDePagos -> dl
+PAGO_T_MONTO EQU 0
+PAGO_T_COMERCIO EQU 8
+PAGO_T_CLIENTE EQU 16
+PAGO_T_APROBADO EQU 17
+PAGO_T_SIZE EQU 24
+
+; cantidadDePagos -> dil
 ; arr_pagos -> rsi
 acumuladoPorCliente_asm:
-	;prólogo
 	push rbp
 	mov rbp, rsp
 	push r12
 	push r13
-	push r14
 
-	;guardo dl y rsi en r12 y r13 así los preservo
-	xor r12, r12
-	mov r12b, dl
-	mov r13, rsi
+	movzx r12, dil ; cantidadDePagos
+	mov r13, rsi ; arr_pagos
+	
+	mov rdi, 10
+	mov rsi, 4
+	call calloc ; array de 10 posiciones de 4 bytes c/u (estructura ocupa 40 bytes)
 
-	;ahora, queremos hacer un call a calloc, preparamos los registros
-	mov rdi, CANTIDAD_DE_CLIENTES
-	mov rsi, SIZE_OF_UINT32
-	call calloc ;en rax se guarda el puntero al array clientes
+	xor r8, r8 ; i = 0
 
-	mov r14, rax ;ahora en r14 tenemos el puntero al array
-
-	.ciclo:
-	;si ya terminamos de recorrer el arreglo que nos pasaron, saltamos a fin
-	cmp r12b, 0
+	.for:
+	cmp r8, r12 ; i == cantidadDePagos ?
 	je .fin
 
-	
+	mov r9, r8 ; r9 = i
+	imul r9, PAGO_T_SIZE ; i * 24
+	add r9, r13 ; r9 = &arr_pagos[i]
 
-	jmp ciclo
+	cmp byte [r9 + PAGO_T_APROBADO], 0
+	je .incrementar
+
+	movzx r10, byte [r9 + PAGO_T_CLIENTE] ; uint8_t cliente = pago.cliente
+	shl r10, 2 ; cliente * 4 (tamanio dato de pagos_aprobados_por_cliente)
+	add r10, rax ; r10 = &pagos_aprobados_por_cliente[cliente]
+	
+	movzx r11, byte [r9 + PAGO_T_MONTO] ; pago.monto
+	add dword [r10], r11d ; pagos_aprobados_por_cliente[cliente] += pago.monto (necesario el dword porque el tamaño del dato del arreglo es de 32 bits, 4 bytes)
+
+	.incrementar:
+	inc r8
+	jmp .for
 
 
 	.fin:
-	;epílogo
-	pop r14
 	pop r13
 	pop r12
 	pop rbp
 	ret
 
 en_blacklist_asm:
+; rdi = comercio
+; rsi = lista_comercios
+; dl = n
+	; push rbp
+	; mov rbp, rsp
+	; push r12
+	; push r13
+	; push r14
+	; push r15
+	; push rbx
+	; sub rsp, 8
+
+	; mov r12, rdi ; r12 = comercio
+	; mov r13, rsi ; r13 = lista_comercios
+	; movzx r14, dl ; r14 = n
+
+	; xor r15, r15 ; i = 0
+
+	; .for:
+	; cmp r15, r14
+	; je .no_esta_en_blacklist
+
+	; mov r9, r15 ; r9 = i
+	; imul r9, 8 ; i * 8
+	; add r9, r13 ; &lista_comercios[i]
+	; mov rdi, [r9] ; rdi = lista_comercios[i]
+
+	; mov rsi, r12 ; rsi = comercio
+	; call strcmp
+	; cmp rax, 0
+	; jne .avanzar
+
+	; jmp .esta_en_blacklist
+
+	; .esta_en_blacklist:
+	; mov rax, 1
+	; jmp .fin
+
+	; .avanzar:
+	; inc r15
+	; jmp .for
+
+	; .no_esta_en_blacklist:
+	; xor rax, rax
+	; .fin:
+	; add rsp, 8
+	; pop rbx
+	; pop r15
+	; pop r14
+	; pop r13
+	; pop r12
+	; pop rbp
 	ret
 
 blacklistComercios_asm:
--- primeros_parciales/primeros_parciales/2023C1R/solucion/ej1/ej1.c
+++ primeros_parciales/primeros_parciales/2023C1R/solucion/ej1/ej1.c
@@ -1,23 +1,49 @@
 #include "ej1.h"
 
 uint32_t* acumuladoPorCliente(uint8_t cantidadDePagos, pago_t* arr_pagos){
-    //creo el arreglo de 10 posiciones e inicializo en 0
-    uint32_t *clientes = calloc(10, sizeof(uint32_t));
-    for (int i= 0; i < cantidadDePagos; i++){
-        //nos vamos a fijar en el monto, en el cliente y en aprobado. si está aprobado el pago, buscamos el cliente y en ese indice en el arreglo que nos construimos vamos a sumarle el monto
-        if (arr_pagos[i].aprobado == 1){
-            uint8_t cliente = arr_pagos[i].cliente;
-            uint8_t monto = arr_pagos[i].monto;
-            clientes[cliente] += monto;
+    uint32_t* pagos_aprobados_por_cliente = calloc(10, sizeof(uint32_t));
+    for (int i = 0; i < cantidadDePagos; i++){
+        pago_t pago = arr_pagos[i];
+        if (pago.aprobado){
+            uint8_t cliente = pago.cliente;
+            pagos_aprobados_por_cliente[cliente] += pago.monto;
         }
     }
-    return clientes;
+    return pagos_aprobados_por_cliente;
 }
 
 uint8_t en_blacklist(char* comercio, char** lista_comercios, uint8_t n){
+    for (int i = 0; i < n; i++){
+        char* comercio1 = lista_comercios[i];
+        if (strcmp(comercio1, comercio) == 0){
+            return 1;
+        }
+    }
+    return 0;
 }
 
 pago_t** blacklistComercios(uint8_t cantidad_pagos, pago_t* arr_pagos, char** arr_comercios, uint8_t size_comercios){
+    // primero vemos la cantidad de pagos tal que su comercio se encuentra blacklisteado
+    uint8_t cant_pagos_res = 0;
+    for (int i = 0; i < cantidad_pagos; i++){
+        pago_t pago = arr_pagos[i];
+        char* comercio = pago.comercio;
+        if (en_blacklist(comercio, arr_comercios, size_comercios)){
+            cant_pagos_res++;
+        }
+    }
+    // ahora armo el array de punteros a los pagos y agrego los pagos
+    pago_t** pagos_res = malloc(cant_pagos_res*24);
+    uint8_t ultimo_agregado = 0;
+    for (int i = 0; i < cantidad_pagos; i++){
+        pago_t pago = arr_pagos[i];
+        char* comercio = pago.comercio;
+        if (en_blacklist(comercio, arr_comercios, size_comercios)){
+            pagos_res[ultimo_agregado] = &arr_pagos[i];
+            ultimo_agregado++;
+        }
+    }
+    return pagos_res;
 }
 
 
