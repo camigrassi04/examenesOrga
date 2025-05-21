48ef9a930e2e17517a3ebb5cc0e38921a28b1505
camigrassi04 <camigras04@gmail.com>
2025-05-21 20:16:55 +0000

    vuelvo a hacer el c

--- primeros_parciales/primeros_parciales/parciales_viejos-2024/1P1C2024/entregable/parcial-bundle.v0.1/solucion/ej1/ej1.c
+++ primeros_parciales/primeros_parciales/parciales_viejos-2024/1P1C2024/entregable/parcial-bundle.v0.1/solucion/ej1/ej1.c
@@ -15,52 +15,43 @@ nodo_display_list_t* inicializar_nodo(
 ordering_table_t* inicializar_OT(uint8_t table_size) {
   ordering_table_t* OT = malloc(sizeof(ordering_table_t));
   OT->table_size = table_size;
-  
-  //creo una tabla con table_size elementos, donde cada uno representa un puntero, por lo que les asigno espacio de 8 bytes.
-  //calloc inicializa en 0
-  if (table_size != 0){
-    nodo_ot_t** table = calloc(table_size, 8);
-    OT->table = table;
-  } else {
+  if (table_size == 0){
     OT->table = NULL;
+  } else {
+    nodo_ot_t** table = calloc(table_size, sizeof(nodo_ot_t*));
+    OT->table = table;
   }
   return OT;
 }
 
 void calcular_z(nodo_display_list_t* nodo, uint8_t z_size) {
-  while(nodo != NULL){
-    uint8_t z = nodo->primitiva(nodo->x, nodo->y, z_size);
-    nodo->z = z;
-    nodo = nodo->siguiente;
+  if (nodo == NULL){
+    return;
   }
+  uint8_t z = nodo->primitiva(nodo->x, nodo->y, z_size);
+  nodo->z = z;
   return;
 }
 
 void ordenar_display_list(ordering_table_t* ot, nodo_display_list_t* display_list) {
-  /*
-  ¿qué hace el algoritmo?
-    - recorre los nodos de la display list
-    - va calculando su z. 
-    - su z indica a qué parte de la ot va a parar. la ot está organizada por los z de los nodos de la display list,
-    - sin embargo, la ot, para el índice z, apunta a una lista enlazada, que tendrá un primer elemento (un nodo_ot_t) y un último
-    - entonces cuando agregamos un nodo de la display list tenemos que, según el z, ver en qué índice de la tabla deberíamos ubicarlo (es decir, en qué lista enlazada) y dónde
-  */
-  calcular_z(display_list, ot->table_size); //calculamos el z de todos los nodos
-  while(display_list != NULL){ //recorremos la lista de nodos display list
-    nodo_ot_t* nuevo_nodo = malloc(sizeof(nodo_ot_t)); // pido espacio para un nuevo nodo
-    nuevo_nodo->display_element = display_list; 
-    nuevo_nodo->siguiente = NULL; // actualizo los campos del nuevo nodo
-    // con ot->table[display_list->z] accedemos al valor de la posición z en la tabla
-    if (ot->table[display_list->z] == NULL){ //no hay primer elemento
-      ot->table[display_list->z] = nuevo_nodo; // como es el primer nodo de la lista enlazada, table[z] deberá apuntar a él.
+  if (ot->table_size == 0) return;
+  while(display_list != NULL){ // recorro la lista de nodos
+    calcular_z(display_list, ot->table_size); // calculo z del nodo
+    // ot es un array de punteros a nodo_ot_t
+    uint8_t i = display_list->z; 
+    nodo_ot_t* nodo = malloc(sizeof(nodo_ot_t));
+    nodo->display_element = display_list;
+    nodo->siguiente = NULL;
+    if (ot->table[i] == NULL){ // no hay primer nodo
+      ot->table[i] = nodo; // table[i] apunta a un nodo_ot_t
     } else {
-      nodo_ot_t* ultimo = ot->table[display_list->z]; 
-      while(ultimo->siguiente != NULL){
-        ultimo = ultimo->siguiente; // me posiciono en el último elemento actual (cuyo siguiente es NULL)
+      nodo_ot_t* ultimo = ot->table[i];
+      while (ultimo->siguiente != NULL){ // busco el último actual
+        ultimo = ultimo->siguiente;
       }
-      ultimo->siguiente = nuevo_nodo; // actualizo el puntero del nodo anterior al que ahora es el último
-    }
-    display_list = display_list->siguiente; // avanzo de nodo
+      ultimo->siguiente = nodo;
+    } 
+    display_list = display_list->siguiente;
   }
   return;
 }
