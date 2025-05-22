8499318a15fbda4799c343fea30243dea82cd7e0
camigrassi04 <camigras04@gmail.com>
2025-05-22 15:19:30 +0000

    vuelvo a hacer 1P1C2024

--- primeros_parciales/primeros_parciales/parciales_viejos-2024/1P1C2024/entregable/parcial-bundle.v0.1/solucion/ej1/ej1.asm
+++ primeros_parciales/primeros_parciales/parciales_viejos-2024/1P1C2024/entregable/parcial-bundle.v0.1/solucion/ej1/ej1.asm
@@ -8,6 +8,13 @@ extern malloc
 extern free
 extern calloc
 
+OFFSET_PRIMITIVA EQU 0
+OFFSET_X EQU 8
+OFFSET_Y EQU 9
+OFFSET_Z EQU 10
+OFFSET_SIGUIENTE_DL EQU 16
+OFFSET_DISPLAY_LIST_SIZE EQU 24 
+
 OFFSET_TABLE_SIZE EQU 0
 OFFSET_TABLE EQU 8
 OFFSET_OT_SIZE EQU 16
@@ -20,31 +27,33 @@ OFFSET_SIZE EQU 16
 
 ; ordering_table_t* inicializar_OT(uint8_t table_size);
 inicializar_OT_asm:
-    ; dil = table_size
+; dil = table_size
     push rbp
     mov rbp, rsp
-
+    
     push r12
     push r13
 
-    xor r12, r12
-    mov r12b, dil
+    movzx r12, dil ; r12 = table_size
+
     mov rdi, OFFSET_OT_SIZE
     call malloc ; rax = OT
-    mov r13, rax
+    mov r13, rax ; r13 = OT
 
-    mov byte [r13 + OFFSET_TABLE_SIZE], r12b ; OT->table_size = table_size
+    mov [r13 + OFFSET_TABLE_SIZE], r12b
 
-    cmp byte r12b, 0
-    je .crear_tabla_null
+    xor rax, rax
+    cmp r12, 0
+    jne .no_vacia
 
-    movzx rdi, r12b
-    mov rsi, 8
-    call calloc ; rax = table 
-    mov [r13 + OFFSET_TABLE], rax
-    jmp .fin
-    .crear_tabla_null:
     mov qword [r13 + OFFSET_TABLE], 0
+    jmp .fin
+
+    .no_vacia:
+    mov rdi, r12
+    mov rsi, OFFSET_SIZE
+    call calloc ; rax = table
+    mov [r13 + OFFSET_TABLE], rax
 
     .fin:
     mov rax, r13
@@ -52,11 +61,11 @@ inicializar_OT_asm:
     pop r12
     pop rbp
     ret
+
 ; void* calcular_z(nodo_display_list_t* display_list) ;
 calcular_z_asm:
-    ; rdi nodo
-    ; sil z_size
-
+; rdi = nodo
+; sil = z_size
     push rbp
     mov rbp, rsp
 
@@ -64,31 +73,22 @@ calcular_z_asm:
     push r13
     push r14
     push r15
-    push rbx
-    sub rsp, 8
 
-    xor r13, r13
     mov r12, rdi ; r12 = nodo
-    mov r13b, sil ; r13b = z_size
+    movzx r13, sil ; r13 = z_size
 
-    .while:
     cmp r12, 0
     je .fin
 
-    mov rbx, [r12] ; rbx = primitiva
-    mov dil, byte [r12 + 8] ; dil = nodo->x
-    mov sil, byte [r12 + 9] ; sil = nodo->y
+    mov r14, [r12 + OFFSET_PRIMITIVA]
+    mov dil, [r12 + OFFSET_X]
+    mov sil, [r12 + OFFSET_Y]
     mov dl, r13b ; dl = z_size
-    call rbx ; rax = z
-    mov byte [r12 + 10], al
-    
-    mov r12, [r12 + 16] ; nodo = nodo->siguiente
 
-    jmp .while
+    call r14 ; rax = z
+    mov byte [r12 + OFFSET_Z], al
 
-    .fin:
-    add rsp, 8
-    pop rbx
+    .fin: 
     pop r15
     pop r14
     pop r13
@@ -97,64 +97,65 @@ calcular_z_asm:
     ret
 ; void* ordenar_display_list(ordering_table_t* ot, nodo_display_list_t* display_list) ;
 ordenar_display_list_asm:
-    ; rdi = ot
-    ; rsi = display_list
-
+; rdi = ot
+; rsi = display_list
     push rbp
     mov rbp, rsp
-
     push r12
     push r13
     push r14
     push r15
     push rbx
+    sub rsp, 8
 
     mov r12, rdi ; r12 = ot
     mov r13, rsi ; r13 = display_list
 
-    ; calculo z para todos los nodos
-    mov rdi, r13
-    movzx rsi, byte [r12 + OFFSET_TABLE_SIZE]
-    call calcular_z_asm
+    cmp byte [r12 + OFFSET_TABLE_SIZE], 0 ; ot->table_size == 0
+    je .fin
 
     .while:
     cmp r13, 0
     je .fin
 
-    mov rdi, OFFSET_SIZE
-    call malloc
-    mov r14, rax ; r14 = nuevo_nodo
+    mov rdi, r13
+    mov sil, byte [r12 + OFFSET_TABLE_SIZE]
+    call calcular_z_asm
 
-    mov [r14 + OFFSET_DISPLAY_ELEMENT], r13 ; nuevo_nodo->display_element = display_list
-    mov qword [r14 + OFFSET_SIGUIENTE], 0 ; nuevo_nodo->siguiente = NULL
+    movzx r14, byte [r13 + OFFSET_Z] ; i = display_list->z
 
-    mov r15, [r12 + OFFSET_TABLE] ; r15 = ot->table
-    movzx rbx, byte [r13 + 10] ; rbx = display_list->z
-    imul rbx, 8 ; multiplico por el tamaño del dato de la table
-    add r15, rbx ; r15 = &ot->table[z]
+    mov rdi, OFFSET_OT_SIZE
+    call malloc ; rax = nodo
 
-    cmp qword [r15], 0 ; ot->table[display_list->z] == NULL ?
-    jne .encontrar_ultimo
+    mov [rax + OFFSET_DISPLAY_ELEMENT], r13 ; nodo->display_element = display_list
+    mov qword [rax + OFFSET_SIGUIENTE], 0 ; nodo->siguiente = NULL
 
-    mov [r15], r14 ; ot->table[display_list->z] = nuevo_nodo
-    jmp .avanzar
+    mov r8, [r12 + OFFSET_TABLE] ; ot->table
+    shl r14, 3 ; i * 8
+    cmp qword [r8 + r14], 0
+    jne .agregar_ultimo
 
-    .encontrar_ultimo:
-    mov rbx, [r15] ; rbx = ultimo
-    .loop:
-    cmp qword [rbx + OFFSET_SIGUIENTE], 0 ; ultimo->siguiente == NULL ?
-    je .insertar_nodo
-    mov rbx, [rbx + OFFSET_SIGUIENTE] ; ultimo = ultimo->siguiente
-    jmp .loop
+    mov [r8 + r14], rax ; ot->table[i] = nodo
+    jmp .siguiente_nodo
 
-    .insertar_nodo:
-    mov [rbx + OFFSET_SIGUIENTE], r14 ; ultimo->siguiente = nuevo_nodo
+    .agregar_ultimo:
+    mov r15, [r8 + r14] ; ultimo = ot->table[i]
+    .buscar_ultimo:
+    cmp qword [r15 + OFFSET_SIGUIENTE], 0
+    je .asignar_nodo
 
-    .avanzar:
-    mov r13, [r13 + 16] ; display_list = display_list->siguiente
+    mov r15, [r15 + OFFSET_SIGUIENTE] ; ultimo = ultimo->siguiente
+    jmp .buscar_ultimo
+
+    .asignar_nodo:
+    mov [r15 + OFFSET_SIGUIENTE], rax ; ultimo->siguiente = nodo
+
+    .siguiente_nodo:
+    mov r13, [r13 + OFFSET_SIGUIENTE_DL] ; display_list = display_list->siguiente
     jmp .while
 
     .fin:
+    add rsp, 8
     pop rbx
     pop r15
     pop r14
